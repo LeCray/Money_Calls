5db0c873e943d504968f59049a6a747ef2097969
LeCray <mistercryptogrid@gmail.com>
2017-08-21 00:48:21 +0200

    Added the particles background

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += [
 	'accounts/Show.js'
 ]
+Rails.application.config.assets.precompile += %w( pages/Particles.js )
 
 Rails.application.config.assets.precompile += %w( accounts/jquery.js )
 
