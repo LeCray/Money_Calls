9565a66d80c6d273a29ab8639fccc2aeacd73df4
LeCray <mistercryptogrid@gmail.com>
2017-08-29 01:06:46 +0200

    Everything works. About to start adding downlines

--- db/migrate/20170828024432_add_referral_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddReferralToClients < ActiveRecord::Migration[5.0]
-  def change
-    add_column :clients, :referral, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822221710) do
+ActiveRecord::Schema.define(version: 20170828024432) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "client_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170822221710) do
     t.datetime "updated_at",        null: false
     t.string   "email"
     t.string   "password_digest"
+    t.string   "referral"
     t.string   "activation_digest"
     t.boolean  "activated"
     t.datetime "activated_at"
